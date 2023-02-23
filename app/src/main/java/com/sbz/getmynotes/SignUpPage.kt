e878ff46e7b1430a169bf470385f6c5a3530338e
SHabaj-dev <shabajansari843@gmail.com>
2023-02-23 22:25:31 +0530

    Sign Up Page Navigation Added

--- app/src/main/java/com/sbz/getmynotes/SignUpPage.kt
+++ app/src/main/java/com/sbz/getmynotes/SignUpPage.kt
@@ -2,10 +2,20 @@ package com.sbz.getmynotes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.sbz.getmynotes.databinding.ActivitySignUpPageBinding
 
+@Suppress("DEPRECATION")
 class SignUpPage : AppCompatActivity() {
+    private lateinit var binding: ActivitySignUpPageBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up_page)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_sign_up_page)
+
+        binding.btnBack.setOnClickListener { onBackPressed() }
+
+        binding.tvLogin.setOnClickListener { onBackPressed() }
+
     }
+
 }

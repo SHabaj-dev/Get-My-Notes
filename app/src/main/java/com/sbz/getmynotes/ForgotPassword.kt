ba03d000b2784cd79931cca37dc6606668f55671
pr847 <Sonukbc6@gmail.com>
2023-02-24 18:31:24 +0530

    added the data binding

--- app/src/main/java/com/sbz/getmynotes/ForgotPassword.kt
+++ app/src/main/java/com/sbz/getmynotes/ForgotPassword.kt
@@ -2,10 +2,17 @@ package com.sbz.getmynotes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.sbz.getmynotes.databinding.ActivityForgotPasswordBinding
 
 class ForgotPassword : AppCompatActivity() {
+    lateinit var binding: ActivityForgotPasswordBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_forgot_password)
+        binding=DataBindingUtil.setContentView(this,R.layout.activity_forgot_password)
+       // setContentView(R.layout.activity_forgot_password)
+
+
     }
 }
